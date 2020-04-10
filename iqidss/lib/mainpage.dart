bfdb0181f292a41f4fa2490f46091b708f45fead
Athirah <nrathrh57@gmail.com>
2020-04-11 03:23:32 +0800

    Notification Dismissal Message

--- iqidss/lib/mainpage.dart
+++ iqidss/lib/mainpage.dart
@@ -252,7 +252,9 @@ class ListTitle extends StatelessWidget {
 }
 
 class Notification extends StatelessWidget {
+  final items = List<String>.generate(5, (i) => "Message ${i + 1}");
 
+  Function setState;
 
   Widget build(BuildContext context) {
     return Scaffold(
@@ -260,11 +262,33 @@ class Notification extends StatelessWidget {
           title: Text("Messages"),
           backgroundColor: Colors.orangeAccent,
         ),
-        body:Card(
+        body: ListView.builder(
+            itemCount: items.length,
+            itemBuilder: (context, index) {
+              final item = items[index];
+              Icon(Icons.delete);
+              return Dismissible(
+                key: Key(item),
+                onDismissed: (direction) {
+                  setState(() {
+                    items.removeAt(index);
+                  });
+                  //Scaffold.of(context).showSnackBar(SnackBar(content: Text("Message deleted")));
+                },
+                background: Container(
+                  child: Center(
+                    child: Text(
+                      'Deleted',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                  color: Colors.red,
+                ),
+                child: Card(
                   color: const Color(0xFFFCE4EC),
                   child: ListTile(
                     leading: Icon(Icons.notifications_active),
-                    title: Text('Message 1'),
+                    title: Text('$item'),
                     subtitle: Text('Collect more score to beat others. '
                         '20/3/2020'),
                     isThreeLine: true,
@@ -272,5 +296,6 @@ class Notification extends StatelessWidget {
                   ),
                 ),
               );
-            }
+            }));
   } // itemBuilder
+}

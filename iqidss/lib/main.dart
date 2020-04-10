5dad16736908cabebdb6c97b23c54c72eb58925b
afifahishakk <afifahishak.md@gmail.com>
2020-04-11 02:08:33 +0800

    main

--- iqidss/lib/main.dart
+++ iqidss/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.yellow,
       ),
       debugShowCheckedModeBanner: false,
-      //home: SplashScreen()
+      home: SplashScreen()
     );
   }
 }

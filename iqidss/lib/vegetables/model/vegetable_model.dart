143a5b733732bdda1e48f3f1ac8d4d31b71bd07a
HanisaNasir <nishahanisa@gmail.com>
2020-05-09 03:36:07 +0800

    insert vegetable_model

---
+++ iqidss/lib/vegetables/model/vegetable_model.dart
@@ -0,0 +1,12 @@
+class VegetableQuestion {
+  String questionString;
+  String firstChoice;
+  String secondChoice;
+  int questionAnswer;
+  
+  // Image image;
+  // AssetImage assetImage;
+String assetImage;
+  VegetableQuestion(this.questionString, this.assetImage, this.firstChoice, this.secondChoice, this.questionAnswer);
+
+}

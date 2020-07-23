6e8898d85f4be72442f74c4675786925409f2573
Nurhasnatul Aida <nurhasnatulaida@gmail.com>
2020-07-23 10:53:32 +0800

    Add Score models

---
+++ lib/score_models/scoredata.dart
@@ -0,0 +1,8 @@
+import 'score.dart';
+
+final globalScoreList = <Score>[
+ Score(id: '1', score: 0), //animal
+ Score(id: '2', score: 0), //color
+ Score(id: '3', score: 0), //shape
+ Score(id: '4', score: 0) //vegetable
+];

a856a41caaa2d22bf62c87f16341cb90e8e26150
Nurhasnatul Aida <nurhasnatulaida@gmail.com>
2020-07-23 10:53:59 +0800

    Add Score Data Services

---
+++ lib/score_services/data_service.dart
@@ -0,0 +1,61 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+
+class DataService {
+    //------- Here is how we implement 'Singleton pattern' in Dart --------
+  static final DataService _instance = DataService._constructor();
+  factory DataService() {
+    return _instance;
+  }
+
+  DataService._constructor();
+  //---------------------------- end of singleton implementation
+
+  // TODO: change the baseUrl to your own REST API service hosted on Firebase (or heroku)
+
+  static const String baseUrl =
+      'https://us-central1-i-qids.cloudfunctions.net/score';
+
+ Future get(String endpoint) async {
+    final response = await http.get('$baseUrl/$endpoint',
+        headers: {'Content-Type': 'application/json'});
+
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    }
+    throw response;
+  }
+
+  Future post(String endpoint, {dynamic data}) async {
+    final response = await http.post('$baseUrl/$endpoint',
+        headers: {'Content-Type': 'application/json'}, body: jsonEncode(data));
+
+    if (response.statusCode == 201) {
+      return jsonDecode(response.body);
+    }
+    throw response;
+  }
+
+  Future patch(String endpoint, {dynamic data}) async {
+    final response = await http.patch('$baseUrl/$endpoint',
+        headers: {'Content-Type': 'application/json'}, body: jsonEncode(data));
+
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    }
+    throw response;
+  }
+
+  Future delete(String endpoint) async {
+    final response = await http.delete('$baseUrl/$endpoint',
+        headers: {'Content-Type': 'application/json'});
+
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    }
+    throw response;
+  }
+
+}
+

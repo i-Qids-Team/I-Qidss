f6a2db7ad4d9d0381d844a76935bc505f2316584
Nurhasnatul Aida <nurhasnatulaida@gmail.com>
2020-07-23 23:17:23 +0800

    Connect ScoreBoard to Firebase

--- lib/Shape/Screen/ScoreBoard.dart
+++ lib/Shape/Screen/ScoreBoard.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:iqidss/mainpage.dart';
+import 'package:iqidss/score_models/score.dart';
+import 'package:iqidss/score_services/score_data_service.dart';
 
 import 'GamePage.dart';
 
@@ -12,8 +14,37 @@ class ScoreBoard extends StatefulWidget {
 }
 
 class _ScoreBoard extends State<ScoreBoard> {
+
+  List<Score> scores = new List<Score>();
+  final dataService = ScoreDataService();
+  Future<List<Score>> _futureData;
+
+   @override
+  void initState() {
+    super.initState();
+     _futureData = dataService.getScoreList();
+  }
+
   Widget build(BuildContext context) {
-    return Scaffold(
+    return FutureBuilder<List<Score>>(
+        future:  _futureData,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            scores = snapshot.data;
+
+            scores[2].id = snapshot.data[2].id;
+            scores[2].score = snapshot.data[2].score;
+
+            scores[2].score = widget.finalscore;
+            dataService.updateScore(id: scores[2].id, score: scores[2].score);
+            return _buildMainScreen(context);
+          } else
+            return _buildFetchingDataScreen();
+        });
+  }
+
+  Scaffold _buildMainScreen(BuildContext context) {
+     return Scaffold(
         backgroundColor: Colors.red[100],
         appBar: AppBar(
           title: Text('Score'),
@@ -59,7 +90,7 @@ class _ScoreBoard extends State<ScoreBoard> {
                       fontSize: 30,
                       color: Colors.black,
                       fontWeight: FontWeight.bold)),
-              Text(widget.finalscore.toString(),
+              Text('${scores[2].score}',
                   style: TextStyle(
                       fontSize: 30,
                       color: Colors.black,
@@ -88,5 +119,20 @@ class _ScoreBoard extends State<ScoreBoard> {
                 ),
               ),
             ])));
+  }
+
+    Scaffold _buildFetchingDataScreen() {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CircularProgressIndicator(),
+            SizedBox(height: 50),
+            Text('Fetching data in progress'),
+          ],
+        ),
+      ),
+    );
   }
 }
